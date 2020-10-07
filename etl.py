a17fb923bd16b990e473c7e042b73fb01a135f58
Oyvind_Elgvin <stoyvind@gmail.com>
2020-10-07 19:35:48 +0200

    fix: new s3 address

--- etl.py
+++ etl.py
@@ -17,13 +17,12 @@ from pyspark.sql.types import IntegerType, TimestampType, DateType
 from pyspark.sql.functions import udf, col, from_unixtime, to_timestamp, monotonically_increasing_id
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
-
+"""
 config = configparser.ConfigParser()
 config.read('dl.cfg')
-
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
-
+"""
 
 def create_spark_session():
     """
@@ -231,7 +230,7 @@ def main():
     spark = create_spark_session()
 
     input_data = "s3a://udacity-dend/"
-    output_data = "s3a://sthreedatalakeudacity/"
+    output_data = "s3a://datalakebucketudacity/"
 
 
     process_song_data(spark, input_data, output_data)
